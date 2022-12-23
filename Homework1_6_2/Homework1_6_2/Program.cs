39c493cba0ee0bddecf085b7bb4b9c308209ec35
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2022-12-23 21:05:21 +0300

    v1

---
+++ Homework1_6_2/Homework1_6_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawPlayer drawPlayer = new DrawPlayer(2, 10,'$');
+
+            drawPlayer.DrawPosition();
+        }
+    }
+
+    class Player
+    {
+        public int PositionX { get; private set; }
+        public int PositionY { get; private set; }
+
+        public Player (int positionX, int positionY)
+        {
+            PositionX = positionX;
+            PositionY = positionY;
+        }
+
+        public void ShowPosition ()
+        {
+            Console.WriteLine("(" + PositionX + "," + PositionY + ")");
+        }
+    }
+
+    class DrawPlayer : Player
+    {
+        private char _symbolPlayer;
+
+        public DrawPlayer(int positionX, int positionY, char symbolPlayer='&') : base(positionX, positionY)
+        {
+            _symbolPlayer = symbolPlayer;
+        }
+
+        public void DrawPosition()
+        {
+            Console.SetCursorPosition(PositionX, PositionY);
+            Console.Write(_symbolPlayer);
+        }
+    }
+}
